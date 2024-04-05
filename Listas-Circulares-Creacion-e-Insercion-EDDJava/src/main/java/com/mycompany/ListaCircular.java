51a1fac41577b9fff84bead15989b3ad37383655
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-04 23:02:53 -0500

    metodo Borrar Lista circular

--- Listas-Circulares-Creacion-e-Insercion-EDDJava/src/main/java/com/mycompany/ListaCircular.java
+++ Listas-Circulares-Creacion-e-Insercion-EDDJava/src/main/java/com/mycompany/ListaCircular.java
@@ -40,4 +40,34 @@ public class ListaCircular {
         return mensaje;
     }
 
+    //metodo para eliminar nodo
+    public boolean eliminarNodo(int elemento) {
+        //creamos un nodo auxiliar
+        NodoCircular nodoActual;
+        boolean encontrado = false;
+        nodoActual = ultimo;
+        //no se ah encontrado
+        while (nodoActual.siguiente != ultimo && !encontrado) {
+            //si ya lo encontro igualamos
+            encontrado = (nodoActual.siguiente.dato == elemento);
+            if (!encontrado) { // si no esta encontrado
+                nodoActual = nodoActual.siguiente;
+            }
+        }
+
+        encontrado = (nodoActual.siguiente.dato == elemento);
+        if (encontrado) {
+            NodoCircular auxiliar = nodoActual.siguiente;
+            if (ultimo == ultimo.siguiente) {
+                ultimo = null;
+            } else {
+                if (auxiliar == ultimo) {
+                    ultimo = nodoActual;
+                }
+                nodoActual.siguiente = auxiliar.siguiente;
+            }
+            auxiliar = null;
+        }
+        return encontrado == true;
+    }
 }
--- Listas-Circulares-Creacion-e-Insercion-EDDJava/src/main/java/com/mycompany/ListasCircularesCreacionEInsercionEDDJava.java
+++ Listas-Circulares-Creacion-e-Insercion-EDDJava/src/main/java/com/mycompany/ListasCircularesCreacionEInsercionEDDJava.java
@@ -3,7 +3,7 @@ package com.mycompany;
 import javax.swing.JOptionPane;
 
 public class ListasCircularesCreacionEInsercionEDDJava {
-
+    
     public static void main(String[] args) {
         int opcion;
         int elemento;
@@ -14,7 +14,8 @@ public class ListasCircularesCreacionEInsercionEDDJava {
                          1. ¿La Lista esta Vacia?
                          2. Agregar Un Elemento
                          3. Mostrar la Lista
-                         4. Salir""";
+                         4. ¿Buscar Elemento y Borrar?
+                         5. Salir""";
         do {
             opcion = Integer.parseInt(JOptionPane.showInputDialog(null, mensaje, "MENU DE OPCIONES", JOptionPane.CLOSED_OPTION));
             switch (opcion) {
@@ -38,12 +39,24 @@ public class ListasCircularesCreacionEInsercionEDDJava {
                     }
                     break;
                 case 4:
+                    elemento = Integer.parseInt(JOptionPane.showInputDialog(null, "¿Ingresa un Nodo a buscar: ?", "Ingresa Ingresa", 2));
+                    if (!listaCircular.estaVacia()) {
+                        if (listaCircular.eliminarNodo(elemento)) {
+                            JOptionPane.showMessageDialog(null, "Elemento: " + elemento + " Borrado", "Borrando Nodo", 2);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Elemento " + elemento + " No Encontrado", "ATENCION", 2);
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Lista Vacia");
+                    }
+                    break;
+                case 5:
                     JOptionPane.showMessageDialog(null, "Gracias por tu participacion", "Bay", JOptionPane.WARNING_MESSAGE);
                     break;
                 default:
                     JOptionPane.showMessageDialog(null, "Elige una opcion Valida", "ATENCION", JOptionPane.CANCEL_OPTION);
                     break;
             }
-        } while (opcion != 4);
+        } while (opcion != 5);
     }
 }
