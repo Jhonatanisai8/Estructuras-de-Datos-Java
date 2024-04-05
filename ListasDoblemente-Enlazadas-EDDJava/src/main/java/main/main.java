c8308d6c29259d13f278858e126f22abe84eb31f
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-04 21:59:32 -0500

    Nuevos Metodos en de esta estructuras de datos listas doblemente enlazadas

--- ListasDoblemente-Enlazadas-EDDJava/src/main/java/main/main.java
+++ ListasDoblemente-Enlazadas-EDDJava/src/main/java/main/main.java
@@ -28,8 +28,22 @@ public class main {
                   6. Eliminar un nodo del Final
                   7. Saber si la Lista esta vacia
                   8. Borrar Todos los Nodos
-                  9. Salir
+                  9. Borrar Nodo espefico
+                 10. Suma de Nodos Positivos
+                 11. Suma de Nodos Negativos
+                 12. Multiplicacion de Nodos positivos
+                 13. Multiplicacion de Nodos negativos
+                 14. ¿Saber cuantos Nodos Positivos hay?
+                 15. ¿Saber cuantos Nodos negativos hay?
+                 16. ¿Saber cual es mayor Nodo?
+                 17. ¿Saber cual es menor Nodo?
+                 18. ¿Saber cuantos Nodos primos hay en la lista? 
+                 19. Encontrar un Nodo en Especifico
+                 20. Primer Nodo
+                 21. Ultimo Nodo
+                 22. Salir
                   ¿Que deseas?""";
+
         do {
             try {
                 opcion = Integer.parseInt(JOptionPane.showInputDialog(null, mensaje, "Menú de Opciones", JOptionPane.INFORMATION_MESSAGE));
@@ -39,7 +53,11 @@ public class main {
                                 JOptionPane.showInputDialog(null, "Ingresa el Elemento del Nodo: ",
                                         "Agregando Nodo al Inicio", JOptionPane.INFORMATION_MESSAGE));
                         lista.Insertar_al_Inicio(elemento);
-
+//                        if (lista.numeroPrimo(elemento) == true) {
+//                            JOptionPane.showMessageDialog(null, "Numero primo");
+//                        } else {
+//                            JOptionPane.showMessageDialog(null, "Numero no es primo");
+//                        }
                         break;
                     case 2:
                         elemento = Integer.parseInt(
@@ -86,8 +104,120 @@ public class main {
                         lista.borrarTodosNodos();
                         break;
                     case 9:
-                        JOptionPane.showMessageDialog(null, "Aplicacion Finalizada", "FIN", JOptionPane.INFORMATION_MESSAGE);
+                        if (lista.Esta_Vacia() == true) {
+                            JOptionPane.showMessageDialog(null, "La lista esta Vacia");
+                        } else {
+                            elemento = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el elemento a buscar: "));
+                            if (lista.deleteSpecificNode(elemento)) {
+                                JOptionPane.showMessageDialog(null, "Nodo eliminado => " + elemento);
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Nodo no encontrado");
+                            }
+                        }
+                        break;
+                    case 10:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "Lista vacia");
+                        } else {
+                            int suma = lista.sumPositiveNodes();
+                            JOptionPane.showMessageDialog(null, "Suma de Nodos positivos es => " + suma);
+                        }
+                        break;
 
+                    case 11:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "Lista vacia");
+                        } else {
+                            int suma = lista.sumNegativeNodes();
+                            JOptionPane.showMessageDialog(null, "Suma de Nodos positivos es => " + suma);
+                        }
+                        break;
+                    case 12:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "La lista esta vacia");
+                        } else {
+                            int multiplicaion = lista.multiplicationPositiveNodes();
+                            JOptionPane.showMessageDialog(null, "Multiplicacion total es => " + multiplicaion, "Multiplicaion de nodos positivos", 2);
+                        }
+                        break;
+                    case 13:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "La lista esta vacia");
+                        } else {
+                            int multiplicaion = lista.multiplicationNegativeNodes();
+                            JOptionPane.showMessageDialog(null, "Multiplicacion total es => " + multiplicaion, "Multiplicaion de nodos Negativos", 2);
+                        }
+                        break;
+                    case 14:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "Lista vacia");
+                        } else {
+                            int contador = lista.counterPositiveNodes();
+                            JOptionPane.showMessageDialog(null, "Total de Nodos Positivos => " + contador);
+                        }
+                        break;
+                    case 15:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "La lista esta Vacia");
+                        } else {
+                            int contador = lista.counterNegativeNodes();
+                            JOptionPane.showMessageDialog(null, "Total de nodos negativos es => " + contador);
+                        }
+                        break;
+                    case 16:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "La lista esta Vacia");
+                        } else {
+                            int numero = lista.majornode();
+                            JOptionPane.showMessageDialog(null, "El nodo mayor es => " + numero);
+                        }
+                        break;
+                    case 17:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "La lista esta Vacia");
+                        } else {
+                            int numero = lista.minorNode();
+                            JOptionPane.showMessageDialog(null, "El nodo menonr es => " + numero);
+                        }
+                        break;
+                    case 18:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "La lista esta Vacia");
+                        } else {
+                            int contador = lista.counterNodesCousins();
+                            JOptionPane.showMessageDialog(null, "Total de Nodos Primos => " + contador);
+                        }
+                        break;
+                    case 19:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "La lista esta vacia");
+                        } else {
+                            elemento = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el Nodo a Buscar"));
+                            if (lista.findNode(elemento)) {
+                                JOptionPane.showMessageDialog(null, "Nodo encontrado");
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Nodo no encontrado");
+                            }
+                        }
+                        break;
+                    case 20:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "Lista Vacia");
+                        } else {
+                            int primer = lista.firstNode();
+                            JOptionPane.showMessageDialog(null, "El primer Nodo de la lista es => " + primer);
+                        }
+                        break;
+                    case 21:
+                        if (lista.Esta_Vacia()) {
+                            JOptionPane.showMessageDialog(null, "Lista Vacia");
+                        } else {
+                            int ultimo = lista.lastNode();
+                            JOptionPane.showMessageDialog(null, "El ultimo Nodo de la lista es => " + ultimo);
+                        }
+                        break;
+                    case 22:
+                        JOptionPane.showMessageDialog(null, "Aplicacion Finalizada", "FIN", JOptionPane.INFORMATION_MESSAGE);
                         break;
                     default:
                         JOptionPane.showMessageDialog(null, "La opcion no esta en el Menu", "Incorrecto",
@@ -97,7 +227,7 @@ public class main {
             } catch (NumberFormatException n) {
                 JOptionPane.showMessageDialog(null, "Error" + n.getMessage());
             }
-        } while (opcion != 9);
+        } while (opcion != 22);
     }
 
 }
