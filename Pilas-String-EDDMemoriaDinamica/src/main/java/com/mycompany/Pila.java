87c8e7ff8150fb3ebab9756b583b4923e3d1a79e
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-20 15:39:47 -0500

    Metodos en la clase Pila

---
+++ Pilas-String-EDDMemoriaDinamica/src/main/java/com/mycompany/Pila.java
@@ -0,0 +1,52 @@
+package com.mycompany;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Pila {
+
+    String vectorPila[];
+    int cima;
+
+    public Pila(int tamanio) {
+        vectorPila = new String[tamanio];
+        cima = -1;
+    }
+
+    public void empujar(String elemento) {
+        cima++;
+        vectorPila[cima] = elemento;
+    }
+
+    public String sacarElemento() {
+        String fuera;
+        fuera = vectorPila[cima];
+        cima--;
+        return fuera;
+    }
+
+    public boolean estaVacia() {
+        return cima == -1;
+    }
+
+    public boolean limpiarPila() {
+        if (estaVacia()) {
+            return false;
+        } else {
+            while (!estaVacia()) {
+                sacarElemento();
+            }
+            return true;
+        }
+    }
+
+    public String mostrarPila() {
+        String men = "";
+        for (int i = cima; i >= 0; i--) {
+            men += "[" + vectorPila[i] + "]";
+        }
+        return men;
+    }
+
+}
---
+++ Pilas-String-EDDMemoriaDinamica/src/main/java/com/mycompany/main.java
@@ -0,0 +1,15 @@
+
+package com.mycompany;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+    
+}
