3e8ce62cc562d8549214e31ada5d14bc3718cff0
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-19 08:43:46 -0500

    constrcutor de la clase nodoArbol

--- Arboles-Binarios-EDD-Java/src/NodoArbol.java
+++ Arboles-Binarios-EDD-Java/src/NodoArbol.java
@@ -2,5 +2,14 @@ public class NodoArbol {
     int dato;
     String nombre;
     NodoArbol hijoIzquierdo,hijoDerecho;
+
+
+    //constructor
+    public NodoArbol(int dato,String nom){
+        this.dato = dato;
+        this.nombre = nom;
+        this.hijoDerecho = null;
+        this.hijoIzquierdo = null;
+    }
     
 }
