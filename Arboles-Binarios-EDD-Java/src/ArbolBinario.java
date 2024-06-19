406f78c951376df22d4e2ab4ee8ff2737ffe2ad3
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-19 08:56:04 -0500

    clase ArbolBinario: metodo agregar un dato al arbol

--- Arboles-Binarios-EDD-Java/src/ArbolBinario.java
+++ Arboles-Binarios-EDD-Java/src/ArbolBinario.java
@@ -1,9 +1,35 @@
 public class ArbolBinario {
     NodoArbol raiz;
 
-    public ArbolBinario(){
+    public ArbolBinario() {
         raiz = null;
     }
 
-    
+    // metodo para insertar un nodo en el arbol
+    public void agregarNodo(int dato, String nombre) {
+        NodoArbol nuevo = new NodoArbol(dato, nombre);
+        if (raiz == null) {
+            raiz = nuevo;
+        } else {
+            NodoArbol auxiliar = raiz;
+            NodoArbol padre;
+
+            while (true) {
+                padre = auxiliar;
+                if (dato < auxiliar.dato) {
+                    auxiliar = auxiliar.hijoIzquierdo;
+                    if (auxiliar == null) {
+                        padre.hijoIzquierdo = nuevo;
+                        return;
+                    }
+                } else {
+                    auxiliar = auxiliar.hijoDerecho;
+                    if (auxiliar == null) {
+                        padre.hijoDerecho = nuevo;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
